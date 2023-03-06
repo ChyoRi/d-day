4fded744c83ab8720e1339c37429d32a7129fdee
ChyoRi <chyori_@naver.com>
2023-03-06 16:47:33 +0900

    third

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 // 전역 변수
 
-const toDay = document.querySelector('.today');
 const toYearSpan = document.querySelector('.to-year');
 const toMonthSpan = document.querySelector('.to-month');
 const toDateSpan = document.querySelector('.to-date');
-const addBtn = document.querySelector('.addition');
+const addModal = document.querySelector('.addmodal');
+const addModalBtn = document.querySelector('.add-modal-btn');
+
+/* Modal 관련 */
+
+const dateTitle = document.querySelector('#date-title');
+const dateInput = document.querySelector('#date');
+const addBtn = document.querySelector('.add');
 
 /* 오늘 날짜 */
 
@@ -18,12 +24,40 @@ toYearSpan.innerText = toYear + '년';
 toMonthSpan.innerText = toMonth + '월';
 toDateSpan.innerText =  toDate + '일';
 
-// D-DAY 생성 함수
+// D-DAY 모달 Open 함수
 
-// const addDday = () => {
-    
-// }
+const addModalOpen = () => {
+    addModal.style.display = 'flex';
+}
+
+// list print
+
+const makeList = (ddayTitle, dday) => {
+    const ddayWrap = document.querySelector('.dday-list-wrap');
+    let list = document.createElement('li');
+    list.classList.add('dday-list');
+    list.innerHtml = `
+        <div class="thumb"></div>
+        <h3 class="dday-title">${ddayTitle}</h3>
+        <div class="dday-wrap">
+            <span class="dday"></span>
+            <span class="criterion">${dday}{</span>
+        </div>
+    `;
+
+    ddayWrap.appendChild(list);
+}
+
+// D-day 추가 함수
+
+const addDate = () => {
+    let ddayTitle = dateTitle.value;
+    let dday = dateInput.value;
+    makeList(ddayTitle, dday);
+    addModal.style.display = 'none';
+}
 
 // 이벤트 리스너
 
-addBtn.addEventListener('click', addDday);
+addModalBtn.addEventListener('click', addModalOpen);
+addBtn.addEventListener('click', addDate);
